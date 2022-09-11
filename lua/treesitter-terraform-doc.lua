c6990b132c62cd1b115c59b071a4a3d0eeaf71ac
Alexandre Fourcat <afourcat@gmail.com>
2022-09-11 13:07:04 +0200

    :lipstick: :books: :art: Split code into function and document them.

--- lua/treesitter-terraform-doc.lua
+++ lua/treesitter-terraform-doc.lua
@@ -3,12 +3,17 @@ local q = require('vim.treesitter.query')
 
 local M = {}
 
-M.version = "0.1.0"
+M.version = "0.2.0"
 M.config = {
     command_name = "OpenDoc",
     url_opener_command = "!open"
 }
 
+---
+--- Get the first parent after root for the current_node
+---
+-- @param current_node node The current used node.
+-- @return node?
 local find_uppest_parent = function(current_node)
     local root = ts_util.get_root_for_node(current_node)
     local parent = current_node:parent()
@@ -26,6 +31,56 @@ local find_uppest_parent = function(current_node)
     return current_node
 end
 
+---
+--- Split a string at the first occurence of a delimiter.
+---
+---@param  s        string  The string to split
+---@param  char     string  The delimiter
+---@param  exclude? boolean Remove the delimiter from the output
+---@return          string  The first part of the string
+---@return          string  The second part of the string
+---@nodiscard
+local split_at_first_occurence = function(s, char, exclude)
+    exclude = exclude or true
+
+    local index = string.find(s, char)
+    local first = string.sub(s, 1, index - 1)
+
+    local second_index = exclude and index + 1 or index
+    local second = string.sub(s, second_index, -1)
+    return first, second
+end
+
+---
+--- Get a dictionary of all the variables in the current node.
+---
+-- @param  query Query   The treesitter query.
+-- @param  node  tsnode  The treesitter node.
+-- @param  bufnr integer The buffer number.
+---@return       table   The dictionary of all match and their text value.
+---@return       integer The length of the dictionary.
+local get_matches_from_node = function(query, node, bufnr)
+    local dict = {}
+    local dict_length = 0
+
+    for _, captures, _ in query:iter_matches(node, bufnr) do
+        for id, capture_node in pairs(captures) do
+            dict_length = dict_length + 1
+            local name = query.captures[id]
+            dict[name] = q.get_node_text(capture_node, bufnr)
+        end
+    end
+    return dict, dict_length
+end
+
+---
+--- Get the terraform resource type and name.
+---
+-- @param node  tsnode  The node in which to look for the resource info.
+-- @param bufnr integer The buffer number.
+---@return      string? The resource type.
+---@return      string? The resource name.
+---@nodiscard
 local get_resource_info = function(node, bufnr)
     local query = vim.treesitter.parse_query('hcl', [[
         (block
@@ -39,41 +94,22 @@ local get_resource_info = function(node, bufnr)
         ) @main
     ]])
 
-    local dict = {}
-    local counter = 0
-    for _, captures, _ in query:iter_matches(node, bufnr) do
-        for id, capture_node in pairs(captures) do
-            counter = counter + 1
-            local name = query.captures[id]
-            dict[name] = q.get_node_text(capture_node, bufnr)
-        end
-    end
+    local dict, dict_length = get_matches_from_node(query, node, bufnr)
 
-    if counter ~= 4 then
+    -- Checks if all captures have matched
+    if dict_length ~= 4 then
         print("Invalid resource targeted, try a 'resource' or 'data' block")
         return nil, nil
     end
 
-    -- split by snake_case
-    local iter = string.gmatch(dict["resource_type"], '[^_]+')
-    local resource_type = iter()
-
-    -- Concat last words
-    local resource_name = ""
-    local next = iter()
-    while next ~= nil do
-        -- Skip the first _
-        if resource_name == "" then
-            resource_name = next
-        else
-            resource_name = resource_name .. "_" .. next
-        end
-        next = iter()
-    end
+    local resource_type, resource_name = split_at_first_occurence(dict["resource_type"], "_")
 
     return resource_type, resource_name
 end
 
+---
+--- Open the terraform documentation from the current cursor position.
+---
 local open_doc_from_cursor_position = function()
     local bufnr = vim.api.nvim_get_current_buf()
     local cursor = ts_util.get_node_at_cursor()
@@ -93,6 +129,11 @@ local open_doc_from_cursor_position = function()
     vim.cmd('silent exec "' .. M.config.url_opener_command .. ' \'' .. url .. '\'"')
 end
 
+---
+--- Setup the configuration for the plugin.
+---   Register the "OpenDoc" (or config.command_name) command.
+---
+--- @param config table The configuration table.
 M.setup = function(config)
     for k, v in pairs(config or {}) do
         M.config[k] = v
